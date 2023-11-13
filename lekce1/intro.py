6264c4ec435349c9f18e912d73acecdd5eeb8548
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-11-13 20:10:07 +0100

    základy done

--- lekce1/intro.py
+++ lekce1/intro.py
@@ -106,3 +106,73 @@ print(a | b)
 print(a - b)
 # xor
 print(a ^ b)
+
+
+# Kontrola behu
+
+vek = 20
+
+if vek < 26:
+    print("Ma mene nez 26 let")
+elif vek > 70:
+    print("stary byk")
+else:
+    print("katastrofe")
+
+    seznam = ["rohliky", "majoneza", "syrecky", "adblue"]
+    for polozka in seznam:
+        print(polozka)
+
+for cislo in range(0, 6):
+    print(cislo)
+
+for cislo in range(2, 10,3):
+    print(cislo)
+
+    x = 10
+    while x <= 15:
+        print(x)
+        x += 1
+
+mocniny = []
+for x in range (10):
+    mocniny.append(x*x)
+
+print("mocniny", mocniny)
+
+mocniny = [x*x for x in range(10)]
+print("Jednodusi", mocniny)
+
+prvky = ["A", "B", "C"]
+seznam = [(x,y) for x in prvky for y in prvky]
+print("seznam", seznam)
+
+
+# import math as m
+import math
+from math import pi
+
+print('PI', math.pi)
+print(pi)
+
+a = 3.3
+print("Ceil",math.ceil(a))
+print("Floor",math.floor(a))
+print("Round",round(3.12345, 3))
+
+# nejvetsi spolecny delited
+print("gcd",math.gcd(20, 12, 60))
+
+print("prod", math.prod([2,3,2]))
+
+import random as rnd
+
+print("random", rnd.random())
+print("uniforn", rnd.uniform(1,5))
+print("randrange",rnd.randrange(5,10))
+
+jmena = ["Petr", "Jana", "Albert", "Lumir"]
+
+print("Choice", rnd.choice(jmena))
+print('Shuffle', rnd.shuffle(jmena))
+
